4fe5fd0dd20e062af401bab51c23297b90b8a59d
pejath <pejath@yandex.ru>
2022-01-27 19:09:41 +0300

    corrected validation for auditorium

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -4,7 +4,7 @@ class Lecture < ApplicationRecord
   belongs_to :lecturer
   belongs_to :subject
 
-  validates :auditorium, numericality: {only_integer: true}, presence: true, uniqueness: { scope: %i[corpus lecture_time_id lecturer_id group_id]}
+  validates :auditorium, numericality: {only_integer: true}, presence: true, uniqueness: { scope: %i[corpus lecture_time_id ]}
   validates :group_id, :lecturer_id, presence: true, uniqueness: {scope: %i[lecture_time_id]}
   validates :corpus, presence: true
 end
