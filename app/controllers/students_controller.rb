249de4076b2ad89b0e359b542ec41c185c61c219
pejath <pejath@yandex.ru>
2022-02-02 15:20:18 +0300

    show, delete, edit for Students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,32 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: %i[show edit update destroy]
+
+  def show
+  end
+
+  def new
+    @students = Student.new
+  end
+
+  def edit
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+  end
+
+  def update
+  end
+
+  def destroy
+    @students.destroy
+
+  end
+
   def red_diplomas
     @students = Student.red_diplomas
+    @groups_id = Group.select(:id)
     @marks = Mark.all
   end
 
@@ -13,5 +39,15 @@ class StudentsController < ApplicationController
                 end
     @groups_id = Group.select(:id)
     @marks = Mark.all
+  end
+
+  private
+
+  def set_student
+    @students = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:group_id, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :lecturers
+  get 'red_diplomas', to: 'students#red_diplomas'
+  get '/journal/(:group)', to: 'students#journal'
+  resources :students, only: %i[destroy new edit show update], path: 'student'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
