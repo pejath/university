a67d29ebba0b5a4b184d3916eb8ce5a5ccc1d149
pejath <pejath@yandex.ru>
2022-02-07 18:41:41 +0300

    All resources for Mark controller and some changes for Student

---
+++ app/controllers/marks_controller.rb
@@ -0,0 +1,80 @@
+class MarksController < ApplicationController
+  before_action :get_student
+  before_action :set_mark, only: %i[ show edit update destroy ]
+
+  # GET /marks or /marks.json
+  def index
+    @marks = @student.marks
+  end
+
+  # GET /marks/1 or /marks/1.json
+  def show
+  end
+
+  # GET /marks/new
+  def new
+    @mark = @student.marks.build
+  end
+
+  # GET /marks/1/edit
+  def edit
+  end
+
+  # POST /marks or /marks.json
+  def create
+    @mark = @student.marks.build(mark_params)
+
+    respond_to do |format|
+      if @mark.save
+        format.html { redirect_to student_marks_path, notice: "Mark was successfully created." }
+        format.json { render :show, status: :created, location: @mark }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @mark.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /marks/1 or /marks/1.json
+  def update
+    respond_to do |format|
+      if @mark.update(mark_params)
+        format.html { redirect_to student_marks_path(@student), notice: "Mark was successfully updated." }
+        format.json { render :show, status: :ok, location: @mark }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @mark.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /marks/1 or /marks/1.json
+  def destroy
+    @mark.destroy
+
+    respond_to do |format|
+      if @mark.destroy
+        format.html { redirect_to student_marks_path(@student), notice: "Mark was successfully destroyed." }
+        format.json { head :no_content }
+      else
+        format.html{ redirect_to student_marks_path, notice: "Something went wrong"}
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def get_student
+    @student = Student.find(params[:student_id])
+    @subject = Subject.select(:id, :name)
+  end
+
+  def set_mark
+    @mark = @student.marks.find(params[:id])
+  end
+
+    # Only allow a list of trusted parameters through.
+  def mark_params
+    params.require(:mark).permit(:subject_id, :mark, :student_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  # resources :lecturers
-  resources :students
+  resources :groups do
+    resources :lectures
+  end
+  resources :students do
+    resources :marks, via: :show
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
