cec562f2a46aef628b3382d47b279c4a4c4786c4
pejath <pejath@yadnex.ru>
2022-02-11 17:52:34 +0300

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  get "/lecture_time", to: 'lecture_times#index'
+
   # resources :lecturers
   resources :students
-
-  resources :lecture_times, via: :show
+  resources :lecture_times, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
